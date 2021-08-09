3a1be4fb9ee09b56306f540bb92decd5daecd0b3
dahai-f <1119369173@qq.com>
2021-08-09 20:37:53 +0800

    35

---
+++ examples/l35.rs
@@ -0,0 +1,33 @@
+use std::cmp::Ordering;
+
+impl Solution {
+    pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
+        let mut left = 0;
+        let mut right = nums.len();
+        while left < right {
+            let mid = left + (right - left) / 2;
+            match nums[mid].cmp(&target) {
+                Ordering::Less => {
+                    left = mid + 1;
+                }
+                Ordering::Equal => {
+                    return mid as i32;
+                }
+                Ordering::Greater => {
+                    right = mid;
+                }
+            }
+        }
+        left as i32
+    }
+}
+
+struct Solution;
+
+fn main() {
+    assert_eq!(Solution::search_insert(vec![1, 3, 5, 6], 5), 2);
+    assert_eq!(Solution::search_insert(vec![1, 3, 5, 6], 2), 1);
+    assert_eq!(Solution::search_insert(vec![1, 3, 5, 6], 7), 4);
+    assert_eq!(Solution::search_insert(vec![1, 3, 5, 6], 0), 0);
+    assert_eq!(Solution::search_insert(vec![1], 0), 0);
+}
