64f9499d7d549a21c77b27dfa6b6fb8aeeeca276
dahai-f <1119369173@qq.com>
2021-08-09 20:01:50 +0800

    704

---
+++ examples/l704.rs
@@ -0,0 +1,30 @@
+use std::cmp::Ordering;
+
+impl Solution {
+    pub fn search(nums: Vec<i32>, target: i32) -> i32 {
+        let mut left = 0;
+        let mut right = nums.len();
+        while left < right {
+            let mid = (left + right) / 2;
+            match nums[mid].cmp(&target) {
+                Ordering::Less => {
+                    left = mid + 1;
+                }
+                Ordering::Equal => {
+                    return mid as i32;
+                }
+                Ordering::Greater => {
+                    right = mid;
+                }
+            }
+        }
+        -1
+    }
+}
+
+struct Solution;
+
+fn main() {
+    let res = Solution::search(vec![-1, 0, 3, 5, 9, 12], 9);
+    println!("{}", res);
+}
