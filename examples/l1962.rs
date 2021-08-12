b79940ab396c851b9b22f852243d55bfb9d6841b
dahai-f <1119369173@qq.com>
2021-08-12 08:22:00 +0800

    [1962. 移除石子使总数最小](https://leetcode-cn.com/problems/remove-stones-to-minimize-the-total/)

---
+++ examples/l1962.rs
@@ -0,0 +1,29 @@
+/// [1962. 移除石子使总数最小](https://leetcode-cn.com/problems/remove-stones-to-minimize-the-total/)
+use std::collections::BinaryHeap;
+
+impl Solution {
+    pub fn min_stone_sum(piles: Vec<i32>, k: i32) -> i32 {
+        let mut piles = BinaryHeap::from(piles);
+        let mut i = 0;
+        while i < k {
+            let max = {
+                match piles.pop() {
+                    None => {
+                        break;
+                    }
+                    Some(max) => max,
+                }
+            };
+            piles.push(max - max / 2);
+            i += 1;
+        }
+        piles.iter().sum()
+    }
+}
+
+struct Solution;
+
+fn main() {
+    assert_eq!(Solution::min_stone_sum(vec![5, 4, 9], 2), 12);
+    assert_eq!(Solution::min_stone_sum(vec![4, 3, 6, 7], 3), 12);
+}
