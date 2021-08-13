1377a3541df682b9b16677b181e7118c081fcd2d
dahai-f <1119369173@qq.com>
2021-08-13 08:35:28 +0800

    [1964. 找出到每个位置为止最长的有效障碍赛跑路线](https://leetcode-cn.com/problems/find-the-longest-valid-obstacle-course-at-each-position/)

--- examples/l1964.rs
+++ examples/l1964.rs
@@ -1,33 +1,19 @@
 /// [1964. 找出到每个位置为止最长的有效障碍赛跑路线](https://leetcode-cn.com/problems/find-the-longest-valid-obstacle-course-at-each-position/)
+use std::cmp::Ordering::{Greater, Less};
 
 impl Solution {
     pub fn longest_obstacle_course_at_each_position(mut obstacles: Vec<i32>) -> Vec<i32> {
-        let mut max_obstacle_pairs: Vec<(i32, i32)> = Vec::with_capacity(obstacles.len());
+        let mut d = Vec::with_capacity(obstacles.len());
         for cur in obstacles.iter_mut() {
-            let mut length = None;
-            for i in (0..max_obstacle_pairs.len()).rev() {
-                let (max, obstacle) = max_obstacle_pairs[i];
-                if obstacle <= *cur {
-                    let new_max = max + 1;
-                    let pos = match max_obstacle_pairs
-                        .binary_search_by_key(&new_max, |(max, _obstacle)| *max)
-                    {
-                        Ok(i) => i,
-                        Err(i) => i,
-                    };
-                    max_obstacle_pairs.insert(pos, (new_max, *cur));
-                    length = Some(new_max);
-                    break;
-                }
+            let point = d
+                .binary_search_by(|x| if *x <= *cur { Less } else { Greater })
+                .unwrap_or_else(|i| i);
+            if point >= d.len() {
+                d.push(*cur);
+            } else {
+                d[point] = *cur;
             }
-            let length = match length {
-                None => {
-                    max_obstacle_pairs.insert(0, (1, *cur));
-                    1
-                }
-                Some(length) => length,
-            };
-            *cur = length;
+            *cur = (point + 1) as i32;
         }
         obstacles
     }
