665911a467bf82d38756f6a44b400cbd2077773c
dahai-f <1119369173@qq.com>
2021-08-14 21:01:31 +0800

    [1963. 使字符串平衡的最小交换次数](https://leetcode-cn.com/problems/minimum-number-of-swaps-to-make-the-string-balanced/)

--- examples/l1963.rs
+++ examples/l1963.rs
@@ -1,7 +1,20 @@
 /// [1963. 使字符串平衡的最小交换次数](https://leetcode-cn.com/problems/minimum-number-of-swaps-to-make-the-string-balanced/)
 
 impl Solution {
-    pub fn min_swaps(s: String) -> i32 {}
+    pub fn min_swaps(s: String) -> i32 {
+        let s = s.as_bytes();
+        let mut min_cnt = 0;
+        let mut cnt = 0;
+        for &c in s.iter() {
+            if c == b'[' {
+                cnt += 1;
+            } else {
+                cnt -= 1;
+                min_cnt = cnt.min(min_cnt);
+            }
+        }
+        (-min_cnt + 1) / 2
+    }
 }
 
 struct Solution;
