6ad41ef710a0b2a79acb8f850a6c37f51460be7d
dahai-f <1119369173@qq.com>
2021-08-15 14:08:08 +0800

    [5844. 数组元素的最小非零乘积](https://leetcode-cn.com/problems/minimum-non-zero-product-of-the-array-elements/)

---
+++ examples/l5844.rs
@@ -0,0 +1,17 @@
+//! [5844. 数组元素的最小非零乘积](https://leetcode-cn.com/problems/minimum-non-zero-product-of-the-array-elements/)
+impl Solution {
+    pub fn min_non_zero_product(p: i32) -> i32 {}
+}
+
+struct Solution;
+
+fn main() {
+    assert_eq!(Solution::rearrange_array(vec![]), vec![]);
+    assert_eq!(
+        Solution::num_of_strings(
+            vec!["a", "b", "c"].iter().map(|&s| s.to_owned()).collect(),
+            "aaaaabbbbb".to_owned(),
+        ),
+        2
+    );
+}
