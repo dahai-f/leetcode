32d0736757a41f09e5fa221d3c380159132b3abc
dahai-f <1119369173@qq.com>
2021-08-15 14:10:35 +0800

    [5845. 你能穿过矩阵的最后一天](https://leetcode-cn.com/problems/last-day-where-you-can-still-cross/)

---
+++ examples/l5845.rs
@@ -0,0 +1,8 @@
+//! [5845. 你能穿过矩阵的最后一天](https://leetcode-cn.com/problems/last-day-where-you-can-still-cross/)
+impl Solution {
+    pub fn latest_day_to_cross(row: i32, col: i32, cells: Vec<Vec<i32>>) -> i32 {}
+}
+
+struct Solution;
+
+fn main() {}
