4662d04d4a07dc18bb5e368cf0c2ef594c8a0915
dahai-f <1119369173@qq.com>
2021-09-07 10:21:54 +0800

    [82. 删除排序链表中的重复元素 II](https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list-ii/)

--- examples/82_删除排序链表中的重复元素_ii.rs
+++ examples/82_删除排序链表中的重复元素_ii.rs
@@ -1,7 +1,5 @@
 //! [82. 删除排序链表中的重复元素 II](https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list-ii/)
 
-use std::cell::UnsafeCell;
-
 // Definition for singly-linked list.
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ListNode {
@@ -35,23 +33,34 @@ impl From<ListNode> for Option<Box<ListNode>> {
 }
 
 impl Solution {
-    pub fn delete_duplicates(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    pub fn delete_duplicates(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
         let mut new_head = None;
-        let new_last = &mut new_head;
+        let mut new_last = &mut new_head;
         let mut has_same = false;
         while let Some(mut cur) = head {
             let next = cur.next.take();
             if let Some(next) = next {
                 if next.val != cur.val {
-                    if has_same {}
+                    if !has_same {
+                        *new_last = Some(cur);
+                        new_last = &mut new_last.as_mut().unwrap().next;
+                    }
+                    has_same = false;
                 } else {
                     has_same = true;
                 }
+                head = Some(next);
+            } else {
+                if !has_same {
+                    *new_last = Some(cur);
+                }
+                head = None;
             }
         }
         new_head
     }
 }
+
 struct Solution;
 
 fn main() {
