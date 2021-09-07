f022240fcddd68fc274abcc74c14a6798a9e068c
dahai-f <1119369173@qq.com>
2021-09-07 08:34:55 +0800

    [162. 寻找峰值](https://leetcode-cn.com/problems/find-peak-element/)

--- examples/162_寻找峰值.rs
+++ examples/162_寻找峰值.rs
@@ -3,14 +3,15 @@
 impl Solution {
     pub fn find_peak_element(nums: Vec<i32>) -> i32 {
         let mut left = 0;
-        let mut right = nums.len();
+        let mut right = nums.len() - 1;
         while left < right {
             let mid = left + (right - left) / 2;
             let mid_num = nums[mid];
-            let mid_left_num = nums.get(mid.wrapping_sub(1));
-            let mid_right_num = nums.get(mid.saturating_add(1));
-            if mid_left_num < mid_num {
-                if mid_num < mid_right_num {}
+            let mid_right_num = *nums.get(mid.saturating_add(1)).unwrap_or(&i32::MIN);
+            if mid_num > mid_right_num {
+                right = mid;
+            } else {
+                left = mid + 1;
             }
         }
         left as i32
@@ -21,5 +22,5 @@ struct Solution;
 
 fn main() {
     assert_eq!(Solution::find_peak_element(vec![1, 2, 3, 1]), 2);
-    assert_eq!(Solution::find_peak_element(vec![1, 2, 1, 3, 5, 6, 4]), 1);
+    assert_eq!(Solution::find_peak_element(vec![1, 2, 1, 3, 5, 6, 4]), 5);
 }
